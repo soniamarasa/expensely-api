2c1a68fa27892c11cf2f59caf72efbc5c079cea0
soniamarasa <soniamarasa@hotmail.com>
2025-05-25 21:11:57 -0300

    feat: main and supabase client config

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from app.routes import user_routes, auth_routes
 
 app = FastAPI()
 
+app.include_router(user_routes.router, prefix="/api", tags=["Usuários"])
+app.include_router(auth_routes.router, prefix="/api", tags=["Auth"])
+
 @app.get("/")
 def root():
-    return {"message": "Expensely API no ar!"}
+    return {"message": "Expensely API is running"}
