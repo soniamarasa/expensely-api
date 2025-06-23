b4ea32960948c70cc89c38858bc70fff815659d9
soniamarasa <soniamarasa@hotmail.com>
2025-06-22 21:46:06 -0300

    fix: main and auth service

--- app/main.py
+++ app/main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
-from app.routes import user_routes, auth_routes, workspaces_routes, expenses_routes, tags_routes
+from fastapi.middleware.cors import CORSMiddleware  
+
+from app.routes import (
+    user_routes,
+    auth_routes,
+    workspaces_routes,
+    expenses_routes,
+    tags_routes,
+)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(user_routes.router, prefix="/api", tags=["Usuários"])
 app.include_router(auth_routes.router, prefix="/api", tags=["Auth"])
--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from app.db.supabase_client import supabase
 
 def login_user(email: str, password: str):
@@ -9,12 +10,31 @@ def login_user(email: str, password: str):
     if response.user is None:
         return {'error': response.error.message if response.error else 'Email ou senha inválidos'}
 
+    user_id = response.user.id
+
+    profile_response = (
+        supabase
+        .table('profiles')
+        .select('*')
+        .eq('id', user_id)
+        .single()
+        .execute()
+    )
+
+    if hasattr(profile_response, 'error') and profile_response.error:
+        raise HTTPException(status_code=404, detail=f"Perfil não encontrado para o usuário {user_id}")
+
+    profile_data = profile_response.data
+
+   
+    user_data = {
+        'id': user_id,
+        'email': response.user.email,
+        **profile_data 
+    }
+
     return {
-        'message': 'Login bem-sucedido',
         'access_token': response.session.access_token,
         'refresh_token': response.session.refresh_token,
-        'user': {
-            'id': response.user.id,
-            'email': response.user.email
-        }
+        'user': user_data
     }
